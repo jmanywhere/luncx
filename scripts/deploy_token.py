9f51245d5870771dcf89dd130dbc81891b7b000d
Jose Miguel Fernandez <josmfernandez@gmail.com>
2022-07-13 15:24:54 -0600

    deploy with pinksale

--- scripts/deploy_token.py
+++ scripts/deploy_token.py
@@ -1,5 +1,12 @@
 from brownie import LuncxToken, IterableMapping, accounts, chain, interface
 
+
 def main():
     owner = accounts.load("dep")
-    token = LuncxToken.deploy("0x3d04e17305A233597e0585E226C3E9174C0ACF4E", {"from":owner}, publish_source=True)
+    IterableMapping.deploy({"from": owner}, publish_source=True)
+    token = LuncxToken.deploy(
+        "0x3d04e17305A233597e0585E226C3E9174C0ACF4E",
+        "0x8EFDb3b642eb2a20607ffe0A56CFefF6a95Df002",
+        {"from": owner},
+        publish_source=True,
+    )
